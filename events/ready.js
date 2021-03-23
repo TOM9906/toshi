0358e20d711fa45f19b9daa66018138c61cff61f
TOM9906 <79160801+TOM9906@users.noreply.github.com>
2021-03-23 09:47:55 +0530

    Delete ready.js

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (client) => {
-  console.log(`[Gaara Music API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("TOM with 69 other", { // Change your bot's status here
-    type: "STREAMING", //can be LISTENING, WATCHING, PLAYING, STREAMING
-  });
-};
